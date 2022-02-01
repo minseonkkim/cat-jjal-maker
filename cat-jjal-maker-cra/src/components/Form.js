bf2d8e9ba65ee11fb87de687b155bca027167163
Minseon Kim <minsun9856@gmail.com>
2022-02-01 15:13:15 +0900

    form css 수정

--- cat-jjal-maker-cra/src/components/Form.js
+++ cat-jjal-maker-cra/src/components/Form.js
@@ -1,15 +1,19 @@
 import React from "react";
+import {useRef} from "react";
 
 const Form = ({ updateMainCat }) => {
     const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
+
+    const line = useRef();
     const [value, setValue] = React.useState("");
     const [errorMessage, setErrorMessage] = React.useState("");
   
     function handleInputChange(e) {
       const userValue = e.target.value;
-      setErrorMessage("");
+      line.current.style.borderColor = "black";
       if (includesHangul(userValue)) {
         setErrorMessage("한글은 입력할 수 없습니다.");
+        line.current.style.borderColor = "red";
       }
       setValue(userValue.toUpperCase());
     }
@@ -17,9 +21,11 @@ const Form = ({ updateMainCat }) => {
     function handleFormSubmit(e) {
       e.preventDefault();
       setErrorMessage("");
+      line.current.style.borderColor = "black";
   
       if (value === "") {
         setErrorMessage("빈 값으로 만들 수 없습니다.");
+        line.current.style.borderColor = "red";
         return;
       }
       updateMainCat(value);
@@ -27,15 +33,16 @@ const Form = ({ updateMainCat }) => {
   
     return (
       <form onSubmit={handleFormSubmit}>
-        <input
+        <input class="ipt"
+          ref={line}
           type="text"
           name="name"
           placeholder="영어 대사를 입력해주세요"
           value={value}
           onChange={handleInputChange}
         />
-        <button type="submit">생성</button>
-        <p style={{ color: "red" }}>{errorMessage}</p>
+        <button class="btn" type="submit">생성</button>
+        <div id="error">{errorMessage}</div>
       </form>
     );
   };
